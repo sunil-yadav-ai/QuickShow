cb980a0e6ebcf3821cba0e902cd88a094fe71eb7
sunil yadav <bca24276117@gmail.com>
2026-09-15 00:27:50 +0530

    home page complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
+import TrailersSection from '../components/TrailerSection'
 const Home = () =>{
     return (
         <>
         <HeroSection/>
         <FeaturedSection/>
+        <TrailersSection/>
             
 
         </>
